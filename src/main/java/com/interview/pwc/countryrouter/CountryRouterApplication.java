0e4586aadf560460c07a068fa9a2cefe7ff1538e
Armin Tirić <atab.craft@gmail.com>
2021-12-01 13:27:03 +0100

    chore: disable security

--- src/main/java/com/interview/pwc/countryrouter/CountryRouterApplication.java
+++ src/main/java/com/interview/pwc/countryrouter/CountryRouterApplication.java
@@ -1,9 +1,12 @@
 package com.interview.pwc.countryrouter;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class})
 public class CountryRouterApplication {
 
 	public static void main(String[] args) {
